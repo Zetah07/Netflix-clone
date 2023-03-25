531ead56c25dcfff4e45c6e04090701b50d942a9
Zetah07 <sebastiancastro0792@gmail.com>
2023-03-25 15:32:33 -0500

    complete login and register whit DB

--- netflix-clone/pages/auth.tsx
+++ netflix-clone/pages/auth.tsx
@@ -1,7 +1,12 @@
+import axios from "axios"
 import { useCallback, useState } from "react"
 import Input from "@/components/input"
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/router"
 
-const auth =() => {
+const Auth =() => {
+  const router = useRouter();
+
   const [email, setEmail] = useState("")
   const [name, setName] = useState("")
   const [password, setPassword] = useState("")
@@ -12,9 +17,31 @@ const auth =() => {
     setVariant((currentVariant) => (currentVariant === "login" ? "register" : "login"))
   }, []);
 
-  const register = useCallback(() => {
-    
-  }, []);
+  const login = useCallback(async () => {
+    try{
+      await signIn("credentials", {
+        email, 
+        password,
+        redirect: false,
+        callbackUrl: "/"
+      });
+      router.push("/");
+    } catch (error) {
+      console.log(error)
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-screen w-screen bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -51,7 +78,9 @@ const auth =() => {
                     value={password}
                     />
                   </div>
-                  <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                  <button
+                  onClick={variant === "login" ? login : register} 
+                  className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                     {variant === "login" ? "Login" : "Sign up"}
                   </button>
                   <p className="text-neutral-500 mt-12">
@@ -69,4 +98,4 @@ const auth =() => {
   )
 }
 
-export default auth
+export default Auth
