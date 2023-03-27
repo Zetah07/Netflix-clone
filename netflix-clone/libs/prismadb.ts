b178928b2f4e5dcc17c91611fef9fec8db6f43af
Zetah07 <sebastiancastro0792@gmail.com>
2023-03-27 13:03:30 -0500

    create profile and progress Navbar & respon

--- netflix-clone/libs/prismadb.ts
+++ netflix-clone/libs/prismadb.ts
@@ -1,6 +1,6 @@
-import {  PrismaClient  }  from  '@prisma/client' ;
+import { PrismaClient } from '@prisma/client'
 
-const client = global.prismadb || new PrismaClient();
-if (process.env.NODE_ENV === 'development') global.prismadb = client;
+const client = global.prismadb || new PrismaClient()
+if (process.env.NODE_ENV !== 'development') global.prismadb = client
 
 export default client;
