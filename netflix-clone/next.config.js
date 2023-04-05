f61b13d9726b662aca1a76737f057550848ec59c
Zetah07 <sebastiancastro0792@gmail.com>
2023-04-05 12:16:24 -0500

    fix deploy

--- netflix-clone/next.config.js
+++ netflix-clone/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  nextAuth: {
-    configFile: 'next-auth.config.js',
-  },
 }
 
 module.exports = nextConfig
